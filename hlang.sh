97730935504b451b65e73d749871951fc8865d14
pierfort123 <pierfort123@users.noreply.github.com>
2015-07-03 19:37:16 +0200

    Update hlang.sh

--- hlang.sh
+++ hlang.sh
@@ -1,6 +1,7 @@
-#!~/bash
-    chmod +x (~/bas.sh)
-    ~/.bash.sh
-    
+  #!/bin/bash
+    /usr/bin/curl --user USERNAME:PASS -s \
+
+    http://jenkinsci/job/PROJECTNAME/build?token=1qaz2wsx
+  
     $ curl -u <username>:<token> https://api.github.com/user
 
