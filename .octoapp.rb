0f8ff6b04b62026e4fe643b3b56c23d38c7abcff
pierfort123 <pierfort123@users.noreply.github.com>
2015-07-16 13:09:33 +0200

    Update .octoapp.rb

--- .octoapp.rb
+++ .octoapp.rb
@@ -4,5 +4,5 @@ require 'octokit'
 # Instead, set and test environment variables, like below
 client = Octokit::Client.new :access_token => ENV['0e8b00befca93f6da8e2e977392da0898ade1901']
 
-results = client.search_code('addClass user:mozilla')
+results = client.search_code('addClass user:pierfort123')
 total_count = results.total_count
