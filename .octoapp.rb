047c4009f0124540dca20aaa90651a94666bbd69
pierfort123 <pierfort123@users.noreply.github.com>
2015-07-17 17:18:19 +0200

    Update .octoapp.rb

--- .octoapp.rb
+++ .octoapp.rb
@@ -1,5 +1,5 @@
 require 'octokit'
-
+gem install jekyll
 # !!! DO NOT EVER USE HARD-CODED VALUES IN A REAL APP !!!
 # Instead, set and test environment variables, like below
 client = Octokit::Client.new :access_token => ENV['0e8b00befca93f6da8e2e977392da0898ade1901']
